e4b175facf8fb785e4fb5115a344af711d8018c7
Brian Hagan <bbhagan@gmail.com>
2020-09-11 23:22:57 -0700

    Packages

--- util/errorLogger.js
+++ util/errorLogger.js
@@ -7,7 +7,7 @@ export const errorLogger = (text) => {
 	const LOGS_DIR = process.env.LOGS_DIR;
 	const ERROR_LOG_FILE = process.env.ERROR_LOG_FILE;
 	try {
-		let writeStream = fs.createWriteStream(`${LOGS_DIR}/${ERROR_LOG_FILE}`, { flags: "a" });
+		const writeStream = fs.createWriteStream(`${LOGS_DIR}/${ERROR_LOG_FILE}`, { flags: "a" });
 		writeStream.write(`${moment().format()} ${text}${os.EOL}`);
 		writeStream.end();
 	} catch (writeError) {
